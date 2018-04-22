13709e6b1031b73e63390dcf1eb154381162deea
root <root@fog.HOME>
2018-04-21 22:17:27 -0700

    updated driver script

--- installdrivers.cmd
+++
@@ -1,3 +0,0 @@
-powershell %HOMEDRIVE%\Drivers\installcerts.ps1
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\UnattendSettings\PnPUnattend\DriverPaths\1" /v "Path" /t REG_SZ /d "C:\Drivers" 
-pnpunattend auditsystem /L > %TEMP%\PnPUnattendLog.txt
