68cc592ee9b169de3d5b7ac0c0759050dce65096
christian <root@home>
2018-05-02 17:43:01 -0700

    now finding files regardless of capitalization

--- mdt_drivers_to_fog.py
+++ mdt_drivers_to_fog.py
@@ -1,7 +1,43 @@
 #!/usr/bin/python3
 import xmltodict
+from os import listdir
 from os.path import dirname, join
 from pathlib import Path, PureWindowsPath
+from re import compile, IGNORECASE
+from fnmatch import translate
+from glob import iglob
+
+#build path database to resolve mispelled paths
+paths = []
+def build_path_db():
+    global paths
+    for filename in iglob('/mdt/**', recursive=True):
+        abs_path = Path(filename).resolve() # get the absolute path
+        rel_path = abs_path.relative_to('/mdt') # convert to relative path    
+        paths.append(rel_path)
+build_path_db()
+
+def unixify_path(win_path):
+    pure_win_path = PureWindowsPath(win_path)
+    #print(pure_win_path)
+    posix_path = Path(pure_win_path)
+
+    #check if path is valid
+    if posix_path.exists():
+        print('already correct!')
+    else:
+        #print('incorrect!')
+        posix_path_str = str(posix_path)
+
+        #computer case insensitive regex pattern for hunted file
+        pattern = compile(translate(posix_path_str), IGNORECASE)
+
+        #check to see if case inensitive pattern matches actual paths
+        for path in paths:
+            if pattern.match(str(path)):
+                #print('match found! %s ' % path)
+                return path
+
 
 driver_groups_xml = '/mdt/Control/DriverGroups.xml'
 driver_xml = '/mdt/Control/Drivers.xml'
@@ -27,36 +63,58 @@ drivers = drivers['drivers']['driver']
 
 for driver in drivers:
     guid = driver['@guid']
+    driver_source = driver['Source']
 
-    source_inf_win = PureWindowsPath(driver['Source'])
-    print('Windows path: %s' % source_inf_win)
-    # TODO convert relative Windows inf path to relative POSIX path
+    print(unixify_path(driver_source))
+
+
+
+
+    #source_inf_win = PureWindowsPath(driver['Source'])
+    #print('Windows path: %s' % source_inf_win)
     # task 1: convert backslashes to forward slashes
-    source_inf_posix_ci = str(Path(source_inf_win))
-    print('Posix path insensitive: %s' % source_inf_posix_ci)
+    #source_inf_posix_ci = Path(source_inf_win)
+    #print('Posix path insensitive: %s' % source_inf_posix_ci)
     # task 2: correct for case senstivity
-    source_inf_posix = source_inf_posix_ci
 
-    print('Posix path: %s' % source_inf_posix)
+    #if source_inf_posix_ci.exists():
+        #print('Exists: %s' % source_inf_posix_ci)
+    #else:
+        #print('Missing: %s' % source_inf_posix_ci)
+
+
+
+
+    #source_inf_posix = source_inf_posix_ci
+
+    #print('Posix path: %s' % source_inf_posix)
 
     #get parent folder
-    source_dir = mdt_root + '/' + dirname(source_inf_posix)
+    #source_dir = mdt_root + '/' + dirname(source_inf_posix)
 
-    driver_guid_sources[guid] = source_dir
-    print('guid %s points to %s' % (guid, source_dir))
+    #driver_guid_sources[guid] = source_dir
+    #print('guid %s points to %s' % (guid, source_dir))
+
+
+
+
+
+
+
+
 
 #   now that we can efficiently convert guids into paths we can build symlinks
 
-driver_groups = driver_groups['groups']['group']
+# driver_groups = driver_groups['groups']['group']
 
-for driver_group in driver_groups:
-    if 'Member' in driver_group: # only process if the group has drivers
-        name = driver_group['Name'] # get the name so we can name our FOG store
-        guids = driver_group['Member'] #list of driver guids
+# for driver_group in driver_groups:
+#     if 'Member' in driver_group: # only process if the group has drivers
+#         name = driver_group['Name'] # get the name so we can name our FOG store
+#         guids = driver_group['Member'] #list of driver guids
         
-        #iterate over the member drivers and create symlinks from MDT to FOG
-        for guid in guids:
-            print('guid %s is found in %s' % (guid, driver_guid_sources[guid]))
-            print('creat symlink to %s' % name)
+#         #iterate over the member drivers and create symlinks from MDT to FOG
+#         for guid in guids:
+#             print('guid %s is found in %s' % (guid, driver_guid_sources[guid]))
+#             print('create symlink to %s' % name)
 
 
