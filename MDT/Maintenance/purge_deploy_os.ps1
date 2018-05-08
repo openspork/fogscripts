09f60eaa9461c047fb9097460c1dd9fdd022765e
christian <root@christian>
2018-05-07 17:30:27 -0700

    add deploy os purge

---
+++ MDT/Maintenance/purge_deploy_os.ps1
@@ -0,0 +1,9 @@
+﻿# Purge existing os
+
+#Script variables
+$root_dir = 'M:'
+$os = 'Windows 10 x64'
+
+Add-PSSnapIn Microsoft.BDD.PSSnapIn
+New-PSDrive -Name 'MDTShare' -PSProvider MDTProvider -Root "$root_dir\MDTFogDeploy" | Out-Null
+Get-ChildItem "MDTShare:\Operating Systems\$($os)" | Remove-Item -Recurse
