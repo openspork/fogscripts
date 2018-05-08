f2115e37c188cf92f80a957237715a018d8acac4
christian <root@home>
2018-05-07 21:48:16 -0700

    added comments

--- mdt_drivers_to_fog.py
+++ mdt_drivers_to_fog.py
@@ -76,44 +76,46 @@ driver_groups = driver_groups['groups']['group']
 
 for driver_group in driver_groups:
     if 'Member' in driver_group: # only process if the group has drivers
-        raw_name = driver_group['Name']
+        raw_name = driver_group['Name'] #pull out the raw Windows style path
 
-        unixy_name = str(Path(PureWindowsPath(raw_name)))
+        unixy_name = str(Path(PureWindowsPath(raw_name))) #convert to posix format
 
-        split_driver_group = split(unixy_name) # get the name so we can name our FOG store
-        model_name = ''.join(split_driver_group[1].split()).lower()
-        make_name = ''.join(basename(split_driver_group[0]).split()).lower()
-        os_name = ''.join(dirname(split_driver_group[0]).split()).lower()
+        split_driver_group = split(unixy_name) #split into components
+        model_name = ''.join(split_driver_group[1].split()).lower() #split out model & strip spaces & uppercase
+        make_name = ''.join(basename(split_driver_group[0]).split()).lower() #split out make & strip spaces & uppercase
+        os_name = ''.join(dirname(split_driver_group[0]).split()).lower() #split out os & strip spaces & uppercase
 
-        print('OS: %s\nMake: %s\nModel: %s\n' % (os_name, make_name, model_name))
+        #print('OS: %s\nMake: %s\nModel: %s\n' % (os_name, make_name, model_name))
         guids = driver_group['Member'] #list of driver guids
         
         #iterate over the member drivers and create symlinks from MDT to FOG
         specific_store = [os_name, make_name, model_name]
 
-
+        #define the base store
         base_store = '/images/drivers/'
 
+        #join the base location with the os, make, model subdirs
         full_store = join(base_store, os_name, make_name, model_name)
 
-        #print(full_store)
-
-        #print(base_store)
+        #create the specific store if not present
         if not exists(base_store):
             makedirs(base_store)
 
+        #handle all driver guids for model
         for guid in guids:
             raw_driver_source = driver_guid_sources[guid]
-            split_driver_source = split(raw_driver_source)
-            driver_type = basename(split_driver_source[0])
-            driver_name = split_driver_source[1]
+            split_driver_source = split(raw_driver_source) #split into components
+            driver_type = basename(split_driver_source[0]) #split out driver time
+            driver_name = split_driver_source[1] #split out driver name
 
-            type_store = full_store + '/' + driver_type
-            driver_store = type_store + '/' + driver_name
+            type_store = full_store + '/' + driver_type #build path to the driver type store
+            driver_store = type_store + '/' + driver_name #build the path to the specific driver store
 
-            if not exists(type_store):
+            #create the type store if not present
+            if not exists(type_store): 
                 makedirs(type_store)
 
+            #symlink from the MDT source to the driver name in driver type store
             symlink(driver_guid_sources[guid], driver_store)
 
 
