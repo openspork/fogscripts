8edcbc916cec9524610ebb523edab85d6bf2800b
christian <root@christian>
2018-05-07 17:31:03 -0700

    begin build TS servicing

---
+++ MDT/Maintenance/update_build_ts.ps1
@@ -0,0 +1,10 @@
+﻿# Updates TS for current Office setup
+# Updates TS for current Ninite setup
+
+#Script variables
+$root_dir = 'M:'
+$os = 'Windows 10 x64'
+$deploy_ts_name = 'deploy'
+
+Add-PSSnapIn Microsoft.BDD.PSSnapIn
+New-PSDrive -Name 'MDTShare' -PSProvider MDTProvider -Root "$root_dir\MDTFogDeploy" | Out-Null
