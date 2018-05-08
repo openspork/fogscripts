0f8e81d48e101e4e7255ab2acd93f0446dbee51c
christian <root@christian>
2018-05-07 18:49:43 -0700

    un hardcode deployment share

--- MDT/Utility/copy_ts.ps1
+++ MDT/Utility/copy_ts.ps1
@@ -1,8 +1,9 @@
 ﻿#Script variables
 $RootDir = 'M:'
+$deployment_share = 'MDTFogDeploy'
 
 Add-PSSnapIn Microsoft.BDD.PSSnapIn
-New-PSDrive -Name "MDTShare" -PSProvider MDTProvider -Root "$RootDir\MDTBuildLab"
+New-PSDrive -Name "MDTShare" -PSProvider MDTProvider -Root "$RootDir\$deployment_share"
 
 $ts_infos = @()
 
@@ -22,15 +23,15 @@ $destination_id = Read-Host -Prompt 'Enter new task sequence ID'
 #$source_id = 'VANILLA-O365BUSINESSRETAIL'
 #$destination_id = 'VANILLA-HOMEBUSINESSRETAIL'
 
-$source_ts_xml = [xml]$(Get-Content "$RootDir/MDTBuildLab/Control/$source_id/ts.xml")
+$source_ts_xml = [xml]$(Get-Content "$RootDir/$deployment_share/Control/$source_id/ts.xml")
 
 $os_guid = ($source_ts_xml | Select-Xml -XPath "//sequence/globalVarList/variable[@name='OSGUID']").Node.'#text'[0]
 
-$os_xml = [xml]$(Get-Content "$RootDir/MDTBuildLab/Control/OperatingSystems.xml")
+$os_xml = [xml]$(Get-Content "$RootDir/$deployment_share/Control/OperatingSystems.xml")
 
 $os_name = $($os_xml | Select-Xml -XPath "//oss/os[@guid='$os_guid']/Name").Node.'#text'
 if ( $os_name -match 'Windows 7' ) { $os_dir = 'Windows 10 x64' }elseif ( $os_name -match 'Windows 8.1' ) { $os_dir = 'Windows 8.1 x64' }elseif ( $os_name -match 'Windows 10' ) { $os_dir = 'Windows 10 x64' }Import-MDTTaskSequence -Template client.xml -name $destination_id -ID $destination_id -OperatingSystemPath `
    "MDTShare:\Operating Systems\$os_dir\$os_name" -Path "MDTShare:\Task Sequences" -Version 1.0
-Copy-Item -Path "$RootDir\MDTBuildLab\Control\$source_id\*" -Destination "$RootDir\MDTBuildLab\Control\$destination_id" -Force
+Copy-Item -Path "$RootDir\$deployment_share\Control\$source_id\*" -Destination "$RootDir\$deployment_share\Control\$destination_id" -Force
 
 Remove-PSDrive MDTShare
