7a333295f23be69a131dd13d74fb27b21cac2ebf
christian <root@christian>
2018-05-30 10:17:57 -0700

    OS version logic now pulls from xml

--- MDT Scripts/Utility/copy_ts.ps1
+++ MDT Scripts/Utility/copy_ts.ps1
@@ -17,11 +17,11 @@ Get-ChildItem -Path "MDTShare:\Task Sequences" | % {
 }
 $ts_infos | Format-Table ID, Name, Template
 
-$source_id = Read-Host -Prompt 'Enter task sequence ID to copy'
-$destination_id = Read-Host -Prompt 'Enter new task sequence ID'
+#$source_id = Read-Host -Prompt 'Enter task sequence ID to copy'
+#$destination_id = Read-Host -Prompt 'Enter new task sequence ID'
 
-#$source_id = 'VANILLA-O365BUSINESSRETAIL'
-#$destination_id = 'VANILLA-HOMEBUSINESSRETAIL'
+$source_id = 'VANILLA-O365BUSINESSRETAIL'
+$destination_id = 'VANILLA-O365PROPLUSRETAIL'
 
 $source_ts_xml = [xml]$(Get-Content "$RootDir/$deployment_share/Control/$source_id/ts.xml")
 
@@ -30,7 +30,7 @@ $os_guid = ($source_ts_xml | Select-Xml -XPath "//sequence/globalVarList/variabl
 $os_xml = [xml]$(Get-Content "$RootDir/$deployment_share/Control/OperatingSystems.xml")
 
 $os_name = $($os_xml | Select-Xml -XPath "//oss/os[@guid='$os_guid']/Name").Node.'#text'
-if ( $os_name -match 'Windows 7' ) { $os_dir = 'Windows 10 x64' }elseif ( $os_name -match 'Windows 8.1' ) { $os_dir = 'Windows 8.1 x64' }elseif ( $os_name -match 'Windows 10' ) { $os_dir = 'Windows 10 x64' }Import-MDTTaskSequence -Template client.xml -name $destination_id -ID $destination_id -OperatingSystemPath `
+$os_build = $($os_xml | Select-Xml -XPath "//oss/os[@guid='$os_guid']/Build").Node.'#text'if ( $os_build -match '10.' ) { $os_dir = 'Windows 10 x64' }#add statements for other build versions IE 6.1 = Win 7 etc.Import-MDTTaskSequence -Template client.xml -name $destination_id -ID $destination_id -OperatingSystemPath `
    "MDTShare:\Operating Systems\$os_dir\$os_name" -Path "MDTShare:\Task Sequences" -Version 1.0
 Copy-Item -Path "$RootDir\$deployment_share\Control\$source_id\*" -Destination "$RootDir\$deployment_share\Control\$destination_id" -Force
 
